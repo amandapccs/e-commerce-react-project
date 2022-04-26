57c09664686231da06eedbee2f91da88ee405c64
juniorhaussler <juniorhaussler@gmail.com>
2022-04-26 17:11:54 -0300

    Requisito 4 concluído.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriesList: [],
+    };
+  }
+
+  async componentDidMount() {
+    const list = await getCategories();
+    this.setState({ categoriesList: list,
+    });
+    console.log(this.list);
+  }
+
   render() {
+    const { categoriesList } = this.state;
     return (
       <div>
+        <aside>
+          <h3>Categorias:</h3>
+          {categoriesList.map((category) => (
+            <label htmlFor={ category.name } key={ category.id } data-testid="category">
+              <input
+                id={ category.name }
+                type="radio"
+                name="category"
+                value={ category.name }
+              />
+              {category.name}
+            </label>
+
+          ))}
+        </aside>
+
         <label htmlFor="pesquisar" data-testid="home-initial-message">
           <input id="pesquisar" />
           Digite algum termo de pesquisa ou escolha uma categoria.
